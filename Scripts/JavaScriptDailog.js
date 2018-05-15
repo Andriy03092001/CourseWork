1a9bcdd0aa9744b210a1f8ee0e6c269e0d185015
Andriy Riabuy <cuanid236316@gmail.com>
2018-05-15 15:51:18 +0300

    first commit

---
+++ Scripts/JavaScriptDailog.js
@@ -0,0 +1,12 @@
+﻿$("#myDialog").hide();
+
+function OpenDialog() {
+    $("#myDialog").show();
+}
+
+function CloseDialog() {
+    $("#myDialog").hide();
+}
+
+$("#btnRegister").on("click", OpenDialog);
+$("#btnCloseDialog").on("click", CloseDialog);
